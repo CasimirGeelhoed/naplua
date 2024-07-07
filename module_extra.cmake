b7b39501d6bbc754f32224570f9b8107f1c7ba8c
CasimirGeelhoed <8721625+CasimirGeelhoed@users.noreply.github.com>
2024-07-08 00:28:21 +0200

    change name to lua52, fix install name

--- module_extra.cmake
+++ module_extra.cmake
@@ -1,2 +1,5 @@
-add_subdirectory(thirdparty/lua)
+add_subdirectory(thirdparty/lua52)
+
 target_link_import_library(${PROJECT_NAME} lua52)
+
+add_license(luabridge ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/LuaBridge/LICENSE.txt)
