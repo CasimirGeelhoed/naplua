ea2e6b37d8474bf557bdf2eb999f8caeee8584ca
CasimirGeelhoed <casimirgeelhoed@gmail.com>
2024-08-24 22:42:19 +0200

    add Lua license

--- module_extra.cmake
+++ module_extra.cmake
@@ -3,3 +3,5 @@ add_subdirectory(thirdparty/lua52)
 target_link_import_library(${PROJECT_NAME} lua52)
 
 add_license(luabridge ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/LuaBridge/LICENSE.txt)
+
+add_license(lua52 ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/lua52/LICENSE.txt)
