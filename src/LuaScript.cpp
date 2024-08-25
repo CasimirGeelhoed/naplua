c6b92496c22e9ff5465b088d7f2c49e7ad7bf5ab
CasimirGeelhoed <casimirgeelhoed@gmail.com>
2024-08-25 10:20:49 +0200

    add license comments

--- src/LuaScript.cpp
+++ src/LuaScript.cpp
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+// Written by Casimir Geelhoed in 2024.
+
 #include "LuaScript.h"
 
 #include <utility/fileutils.h>
